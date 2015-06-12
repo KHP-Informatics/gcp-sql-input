557abc89c55f129a4d86475978a4b257c4daf121
Cass Johnston <caroline.johnston@kcl.ac.uk>
2015-06-12 22:19:06 +0100

    junit skeleton

--- test/DoTest.java
+++ test/DoTest.java
@@ -3,10 +3,16 @@ import static org.junit.Assert.* ;
 
 import gate.cloud.io.sql.SQLInputHandler;
 
-class DoTest {
- public static void main(String[] args){
-    
-     //SQLInputHandler sh = new SQLInputHandler();
-       
+public class DoTest {
+  
+  protected SQLInputHandler sq;
+
+  /* public static void main(String[] args){  
+    SQLInputHandler sh = new SQLInputHandler();
+     }
+  */
+  @Test
+  public void testTest(){
+    assertEquals(1,1);
   }
 }
